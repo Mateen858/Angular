46443ac3a883efa90febe6085c6fb5da18db8d86
Rana Mateen <a.mateen858@hotmail.com>
2019-04-16 00:04:53 +0500

    customer service completed

---
+++ first-project/src/app/core/core.module.ts
@@ -0,0 +1,13 @@
+
+import {NgModule} from "@angular/core";
+
+import { CustomerService } from "./customer.service";
+
+ @NgModule({
+    imports: [],
+    providers: []
+})
+
+export class CoreModule{
+
+}
---
+++ first-project/src/app/core/customer.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from "@angular/core";
+
+import { Observable, from } from 'rxjs';
+
+import { HttpClient } from "@angular/common/http";
+import { map, catchError } from 'rxjs/operators'
+
+
+@Injectable()
+export class CustomerService{
+
+    constructor(private http: HttpClient){    }
+get Customers () : Observable<any[]>{
+    return this.http.get<any[]>("../assets/customer.json").pipe(
+catchError(this.handleError)
+
+    );
+}
+
+
+getCustomer(id: number) : Observable<any> {
+    return this.http.get<any[]>("../assets/customer.json")
+      .pipe(
+        map(customers => {
+          let customer = customers.filter((cust: any) => cust.id === id);
+          return (customer && customer.length) ? customer[0] : null;
+        }),
+        catchError(this.handleError)
+      )
+  }
+    private handleError (error:any){
+
+        
+        if (error.error instanceof Error){
+            console.log("error messgae:", error.error.messgae)
+            return Observable.throw (error.error.messgae);
+            
+        }
+        console.log("Server error", error);
+        return Observable.throw(error || 'Node.js server error');
+    }
+
+
+}
